d87077ae946f23d735253996f9a0e6f53f5b9592
sahil-1234 <sahil.srivastava0170@gmail.com>
2022-02-05 23:56:02 +0530

    half

--- App.py
+++ App.py
@@ -1,7 +1,6 @@
-from distutils.command.build import build
-import tkinter
-from tkinter import Frame, ttk
-class AlgorithmVisualizer(tkinter.Tk):
+
+from tkinter import Frame, ttk, Tk
+class AlgorithmVisualizer(Tk):
     def __init__(self):
         super().__init__()
         self.title('Algorithm Visualizer')
@@ -10,15 +9,16 @@ class AlgorithmVisualizer(tkinter.Tk):
     def create_algorithm_buttons(self):
         button_frame=ttk.Frame(self)
         button_frame.place(rely=0.5)
-        game_of_life=ttk.Button(button_frame,text='Game of Life',width=self.width)
-        game_of_life.grid(row=1,column=1)
-        towers_of_hanoi=ttk.Button(button_frame,text='Towers of Hanoi',width=400)
-        towers_of_hanoi.grid(row=2,column=1)
-        selection_sort=ttk.Button(button_frame,text='Selection Sort',width=400)
-        selection_sort.grid(row=3,column=1)
-        quick_sort=ttk.Button(button_frame,text='Quick Sort',width=400)
-        quick_sort.grid(row=4,column=1)
-        bubble_sort=ttk.Button(button_frame,text='Bubble Sort',width=400)
-        bubble_sort.grid(row=5,column=1)
+        game_of_life=ttk.Button(button_frame,text='Game of Life',width=50)
+        game_of_life.grid(row=0,column=0)
+        self.config(bg = "skyblue")      
+        towers_of_hanoi=ttk.Button(button_frame,text='Towers of Hanoi',width=50)
+        towers_of_hanoi.grid(row=1,column=1)
+        selection_sort=ttk.Button(button_frame,text='Selection Sort',width=50)
+        selection_sort.grid(row=0,column=2)
+        quick_sort=ttk.Button(button_frame,text='Quick Sort',width=50)
+        quick_sort.grid(row=1,column=3)
+        bubble_sort=ttk.Button(button_frame,text='Bubble Sort',width=50)
+        bubble_sort.grid(row=0,column=4)
 if __name__=='__main__':
     AlgorithmVisualizer().mainloop()
